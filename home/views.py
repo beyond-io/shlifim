68dc55ade9b0a69f5be7017bfeb2ef813e427750
Aviv Zafrani <avivz450@gmail.com>
2021-04-15 20:10:11 +0300

    Edited home/urls.py home/views.py
    - Added new_question method for submitting a question in home/views.py file
    - Added a path to the new_question in home/urls.py file

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import QuestionForm
+from .models import Profile
 
 
 def about(request):
@@ -7,3 +9,17 @@ def about(request):
 
 def landingpage(request):
     return render(request, 'home/landingpage.html')
+
+
+def new_question(request):
+    form = QuestionForm
+    if request.method == 'POST':
+        questForm = QuestionForm(request.POST)
+        if questForm.is_valid():
+            questForm = questForm.save(commit=False)
+            for currProfile in Profile.objects.all():
+                if (currProfile.user == request.user):
+                    questForm.profile = currProfile
+                    break
+            questForm.save()
+    return render(request, 'home/questions/new_question.html', {'form': form, 'title': 'New Question'})
