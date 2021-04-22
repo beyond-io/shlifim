bb278c24f1fba3bdecbe7de97a89c9ed92e21e14
Danit Levi <danitLevi2708@gmail.com>
2021-04-22 17:11:26 +0300

    Creating explore page
    -creating explore page which displays all questions in the app.
    -update model.py file:
    *creating  "get_answers_num" method in Question model
    
    -updating the url.py file and create an "explore" URL.
    -updating the view.py file and creating QuestionsListView class
    -create explore.html template file

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import QuestionsListView
 
 urlpatterns = [
     path('about/', views.about, name='about'),
-    path('', views.landingpage, name='landingpage')
+    path('', views.landingpage, name='landingpage'),
+    path('explore/', QuestionsListView.as_view(), name='explore-page')
 ]
