3f0e05ab4401a761baea021f92ce7c0a78e3aa48
Danit Levi <danitLevi2708@gmail.com>
2021-04-22 17:11:26 +0300

    Creating explore page
    -creating explore page which displays all questions in the app.
    -update model.py file:
    *creating  "get_answers_num" method in Question model
    
    -updating the url.py file and create an "explore" URL.
    -updating the view.py file and creating QuestionsListView class
    -create explore.html template file

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Question, Tag
+from .models import Question
+from django_filters.views import FilterView
 
 
 def about(request):
@@ -30,3 +32,26 @@ def tags(request):
     else:
         tags = Tag.tags_feed()
     return render(request, 'home/tags.html', {'tags': tags})
+
+class QuestionsListView(FilterView):
+    model = Question
+    template_name = 'home/explore.html'
+    context_object_name = 'questions'
+    ordering = ['-publish_date']
+    paginate_by = 10
+    filterset_class = QuestionFilter
+
+    def get_paginate_by(self, queryset):
+        return self.request.GET.get("paginate_by", self.paginate_by)
+
+    def get_queryset(self):
+        items_set = Question.objects.all()
+        ordering = self.request.GET.get('order_by', '-publish_date')
+        if ordering == "answersNum":
+            items_set = items_set.annotate(answers_num=Count('answer')).order_by('-answers_num')
+        else:
+            items_set = items_set.order_by(ordering)
+
+        items_set = QuestionFilter(self.request.GET, queryset=items_set)
+
+        return items_set.qs
