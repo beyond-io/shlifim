38f78e0ac515e741429b065dd4a24b1e5c7563ac
Danit Levi <danitLevi2708@gmail.com>
2021-04-22 17:50:04 +0300

    Adding filter option in "explore page"
    -Enable filtering questions in "explore page" by :
     *Subject
     *Sub subject
     *Grade
     *Book
     *Book page
    
    -add min and max value to the book_page filed so the user will not be able to filter book page by negative value or value bigger than 9999

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Question
 from django_filters.views import FilterView
 from django.db.models import Count
+from .filters import QuestionFilter
 
 
 def about(request):
@@ -18,6 +19,7 @@ class QuestionsListView(FilterView):
     context_object_name = 'questions'
     ordering = ['-publish_date']
     paginate_by = 10
+    filterset_class = QuestionFilter
 
     def get_paginate_by(self, queryset):
         return self.request.GET.get("paginate_by", self.paginate_by)
@@ -30,4 +32,6 @@ class QuestionsListView(FilterView):
         else:
             items_set = items_set.order_by(ordering)
 
-        return items_set
+        items_set = QuestionFilter(self.request.GET, queryset=items_set)
+
+        return items_set.qs
