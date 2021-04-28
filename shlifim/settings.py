412d4851217b944494fb025c4b9b4b33b354a73d
Aviv Zafrani <avivz450@gmail.com>
2021-04-28 14:22:29 +0300

    Included the CKEditor in the relevant places
    - Included the editor in shlifim/settings.py at INSTALLED_APPS
    Also, added a setting to avoid irrelevant comments in migrate command
    - Included the editor into new_question.html to show the editor to the user
    - Included the editor inside Pipfile and Pipfile.lock
    Made it to update the dependencies
    Made it by doing the command "pipenv install django-ckeditor" after installation

--- shlifim/settings.py
+++ shlifim/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
