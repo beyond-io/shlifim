2d5bdf64bc4d73974e0b5f36c757ce68b865d7d5
Aviv Zafrani <avivz450@gmail.com>
2021-04-28 14:22:59 +0300

    Edited models.py and added migration file for that
    Edited the content field in the Question model to add the editor
    The new migration file for that change - 0015_alter_question_content.py

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from ckeditor.fields import RichTextField
 
 
 class Gender(models.TextChoices):
@@ -73,7 +74,7 @@ class Grade(models.TextChoices):
 class Question(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    content = models.TextField()
+    content = RichTextField(blank=True, null=True)
     publish_date = models.DateTimeField(default=timezone.now)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     sub_subject = models.ForeignKey(Sub_Subject, on_delete=models.CASCADE, blank=True, null=True)  # field not required
